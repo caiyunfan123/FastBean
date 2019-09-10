3bbf20b30624dddc6fceeccfbb55db873f694452
cyf123 <516691435@qq.com>
2019-09-10 08:52:52 +0800

    Version 2.0

--- FastBean.scala
+++ FastBean.scala
@@ -1,30 +1,66 @@
 package utils
 
-trait FastBean {
-  def build(arr:Array[String]) = FastBean.build(this)(arr)
-
-}
 object FastBean{
-  def build[T](obj:T)(arr:Array[String]) ={
+  def typeEquals[T](obj:T,arr:Array[String]):Boolean= equalsMethod(obj,arr,isEqualsAll = true)
+  def lengthEqulas[T](obj:T,arr:Array[String]):Boolean= equalsMethod(obj,arr,isEqualsAll = false)
+
+  def build[T](obj: T,arr: Array[String]): T = {
     var index = 0
-    obj.getClass.getDeclaredFields.foreach(field=>{
-      field.setAccessible(true)
-      try {
-        field.getType.getTypeName match {
-          case "java.lang.String" => field.set(obj, arr(index))
-          case "int" => field.set(obj, arr(index).toInt)
-          case "float" => field.set(obj, arr(index).toFloat)
-          case "double" => field.set(obj, arr(index).toDouble)
-          case "boolean" => field.set(obj, arr(index).toBoolean)
-          case "short" => field.set(obj, arr(index).toShort)
-          case "byte" => field.set(obj, arr(index).toByte)
-          case _ => throw new RuntimeException("不支持的类型")
-        }
-      } catch{
-        case a:NumberFormatException =>throw new RuntimeException(s"第${index}个参数，${arr(index)}无法正确注入${field.getName}，建议检查参数是否完全对应")
-      }
-      index=index+1
-    })
+    val fields = obj.getClass.getDeclaredFields
+    if (fields.length != arr.length) throw new RuntimeException(s"注入的参数数量与参数总数不符${fields.length}!=${arr.length}")
+    try {
+      transform(obj, arr)
+    }
+    catch {
+      case _: NumberFormatException =>
+        throw new RuntimeException(
+          s"${index}/${arr.length}，${arr(index)}无法正确注入${fields(index).getName}，建议检查参数是否完全对应\n" +
+            s"${arr.mkString(",")}"
+        )
+    }
     obj
+  }
+  def buildOrElse[T](obj:T,arr:Array[String],orElse:T):T={
+    var index = 0
+    val fields = obj.getClass.getDeclaredFields
+    if (fields.length != arr.length) return orElse
+    try {
+      transform(obj, arr)
+    }
+    catch {
+      case _: NumberFormatException => return orElse
+    }
+    obj
+  }
+
+  private def equalsMethod[T](obj: T, arr: Array[String], isEqualsAll: Boolean): Boolean = {
+    val fields = obj.getClass.getDeclaredFields
+    if (fields.length != arr.length) return false
+    if (!isEqualsAll) return true
+    try {
+      transform(obj, arr)
+    }
+    catch {
+      case _: NumberFormatException => return false
+    }
+    true
+  }
+
+  private def transform[T](obj: T, arr: Array[String]): Unit = {
+    var index = 0
+    obj.getClass.getDeclaredFields.foreach(field => {
+      field.setAccessible(true)
+      field.getType.getTypeName match {
+        case "java.lang.String" => field.set(obj, arr(index))
+        case "int" => field.set(obj, arr(index).toInt)
+        case "float" => field.set(obj, arr(index).toFloat)
+        case "double" => field.set(obj, arr(index).toDouble)
+        case "boolean" => field.set(obj, arr(index).toBoolean)
+        case "short" => field.set(obj, arr(index).toShort)
+        case "byte" => field.set(obj, arr(index).toByte)
+        case _ => throw new RuntimeException("不支持的类型")
+      }
+      index = index + 1
+    })
   }
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # FastBean
 
+### version 1.0
+
 case class Bean(
 var a:Int = 0
 var b:String = null
@@ -9,15 +11,29 @@ var c:Double = 0
 main{
 val str = "123,abc,2.2"
 
-val bean =FastBean.build(Bean())(str.split(","))
+val bean =FastBean.build(Bean(),str.split(","))
 
 }
-
-
 
 备注：
 1.带有空参构造器的普通class类可以不给属性默认值，否则必须如上给个默认值。
 2.属性必须都是可变的var
 3.支持除char外的七个基本类型和String
 4.当字段不匹配时，会抛出异常，并提示出错的数据所在的位置和对应的字段名
+
+### version 2.0
+
+##### 新增
+
+buildOrElse方法：传入一个默认值，当属性注入失败时会返回该默认值，可用于后续filter过滤
+
+lengthEqulas方法：比较对象参数与所给参数的参数个数是否一致
+
+typeEqulas方法：除了比较个数以外，还会比较每个类型是否一致
+
+##### 更新
+
+build方法现在会详细提示注入失败的完整数据以及失败的参数所在位置
+
+API均大幅度改动，原1.0版本弃用。
 
